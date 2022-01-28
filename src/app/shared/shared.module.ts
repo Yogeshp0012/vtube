13d28f1278161c080ad6b1e3774c649d6dd751f5
P YOGESH <yogeshp0012@gmail.com>
2022-01-28 21:17:17 +0530

    Add FormControl to Authentication Forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ModalComponent } from './modal/modal.component'
 import { TabsContainerComponent } from './tabs-container/tabs-container.component'
-import { TabComponent } from './tab/tab.component'
+import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component'
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
   imports: [CommonModule],
   exports: [ModalComponent, TabsContainerComponent, TabComponent],
 })
