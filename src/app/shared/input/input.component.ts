639421a6829e08ca64c91fb9f289a39bd2a7dbc6
Yogeshp0012 <yogeshp0012@gmail.com>
2022-01-31 21:17:36 +0530

    Validate Forms

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
+  @Input() control: FormControl = new FormControl()
+  @Input() type = 'text'
+  @Input() placeholder = ''
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
